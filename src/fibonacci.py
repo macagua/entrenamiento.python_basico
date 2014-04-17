eaebca8ebc43f616d947fa93d3d54aee0ef609b1
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2014-04-17 11:14:34 -0430

    Actualizado funcionalidad y documentacion de serie fibinacci

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,18 +1,26 @@
 # -*- coding: utf8 -*-
 
-# módulo de números Fibonacci
+"""
+    módulo de Sucesión de números Fibonacci
+    Mas informacion en http://es.wikipedia.org/wiki/Sucesi%C3%B3n_de_Fibonacci
+"""
 
-def fib(n):    # escribe la serie Fibonacci hasta n
+# escribe la sucesión Fibonacci hasta n
+def imprime_fibonacci(n):
     a, b = 0, 1
     while b < n:
         print b,
-        a, b = b, a+b
+        a, b = b, a + b
 
-def fib2(n): # devuelve la serie Fibonacci hasta n
+# devuelve la sucesión Fibonacci hasta n
+def devuelve_fibonacci(n): 
     resultado = []
     a, b = 0, 1
     while b < n:
         resultado.append(b)
-        a, b = b, a+b
+        a, b = b, a + b
     return resultado
 
+print "La sucesión Fibonacci hasta 10 es:", imprime_fibonacci(10)
+
+print "La sucesión Fibonacci hasta 50 es:", devuelve_fibonacci(50)
