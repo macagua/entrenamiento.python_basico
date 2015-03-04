a083bdce7ff85ac4bd5f20f2aaff9feac4a2a892
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2015-03-03 20:18:37 -0430

    Agregar modulo python operadores_relacionales.py

---
+++ manual-de-curso/source/leccion4/operadores_relacionales.py
@@ -0,0 +1,44 @@
+a = 5
+b = 5
+a1 = 7
+b1 = 3
+c1 = 3
+
+cadena1 = 'Hola'
+cadena2 = 'Adios'
+
+lista1 = [1, 'Lista Python', 23]
+lista2 = [11, 'Lista Python', 23]
+
+# igual
+c = a == b
+print c
+
+cadenas = cadena1 == cadena2
+print cadenas
+
+listas = lista1 == lista2
+print listas
+
+# diferente
+d = a1 != b
+print d
+
+cadena0 = cadena1 != cadena2
+print cadena0
+
+# mayor que
+e = a1 > b1
+print e
+
+# menor que
+f = b1 < a1
+print f
+
+# mayor o igual que
+g = b1 >= c1
+print g
+
+# menor o igual que
+h = b1 <= c1
+print h
