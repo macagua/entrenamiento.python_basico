c0805c6090e8da9ef9bd8e188353220b2579dd26
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2015-03-04 17:13:20 -0430

    Agregar programa a depurar

---
+++ manual-de-curso/source/leccion6/funcion_a_depurar.py
@@ -0,0 +1,11 @@
+import pdb
+
+def calculo(cedula, *args):
+    pdb.set_trace()
+    if args[0]:
+        print args[0]
+    elif args[1]:
+    	print args[1]
+    print cedula
+
+calculo(14522590, 12, 2334)
