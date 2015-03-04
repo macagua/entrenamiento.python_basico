3028f7c537b84d867c13eaf6533c56a9e152a19b
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2015-03-04 18:47:59 -0430

    Agregar nuevas configuraciones de imagenes para Web y PDF

--- manual-de-curso/source/conf.py
+++ manual-de-curso/source/conf.py
@@ -32,7 +32,6 @@ extensions = [
 	'sphinx.ext.todo',
 	'sphinx.ext.viewcode',
 	'sphinx.ext.intersphinx',
-#	'ipython_console_highlighting',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -99,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -111,19 +110,19 @@ html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = u'Materiales del curso de programación en Python - Nivel básico'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = u'Programación en Python - Nivel básico'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_static/covantec-logo.jpg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = '_static/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -220,7 +219,8 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = 'covantec.jpg'
+#latex_logo = 'covantec.jpg'
+latex_logo = '_static/covantec.jpg'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
