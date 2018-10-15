57024973636f9d2ea3198e0bec95ff073c84fdda
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-10-15 08:40:36 -0400

    Agregar la extensión IPython.sphinxext

--- source/conf.py
+++ source/conf.py
@@ -32,6 +32,8 @@ extensions = [
 	'sphinx.ext.todo',
 	'sphinx.ext.viewcode',
 	'sphinx.ext.intersphinx',
+	'IPython.sphinxext.ipython_console_highlighting',
+	'IPython.sphinxext.ipython_directive'
 ]
 
 # Options for the linkcheck builder
