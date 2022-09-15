0e5262e8e3d7cf792fbfde8264d992365759fb3a
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2022-09-15 09:48:51 -0400

    Agregar salto de lineas al final a los archivos de js y css

--- source/_static/javascripts/virtual_business_card.js
+++ source/_static/javascripts/virtual_business_card.js
@@ -2,7 +2,7 @@
 $('body').on('click', '.icon', function() {
     var element = $(this).children().last();
     console.log(element);
- 
+
     element.css('animation', 'ripple 1s ease-out');
     setTimeout(function() {
         element.css('animation', '');
