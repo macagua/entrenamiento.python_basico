e1c7476f27fbc5c2e93bce792c943237a6232f1d
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2022-09-27 10:30:53 -0400

    Agregar estilos personalizados a la extension sphinx_contributors

--- source/conf.py
+++ source/conf.py
@@ -239,6 +239,7 @@ html_css_files = [
     "https://netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "stylesheets/virtual_business_card.css",
     "stylesheets/soporte_profesional.css",
+    "stylesheets/sphinx_contributors.css",
     "stylesheets/main/layout/_content.css",
     "stylesheets/main/layout/_footer.css",
     # 'https://example.com/css/custom.css',
